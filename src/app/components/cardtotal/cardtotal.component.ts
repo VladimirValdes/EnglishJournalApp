47d717921c03b8b0fd4d9078bdccc0f66ff02ee0
Vladimir <mendozahum20@gmail.com>
2022-01-04 12:14:17 -0600

    feat: change colors in cardTotal

--- src/app/components/cardtotal/cardtotal.component.ts
+++ src/app/components/cardtotal/cardtotal.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cardtotal',
-  templateUrl: './cardtotal.component.html',
+  template: `
+  <div class="d-flex justify-content-center align-items-center card shadow" style="width: 18rem; boder-radius: 5px">
+    <div class="card-body">
+      <h5 class="card-title text-center fw-bolder" style="font-size: 5rem;" [style.color]="color">{{ number }}</h5>
+      <p class="card-text text-center fs-2" [style.color]="color">{{ name }}</p>
+    </div>
+  </div>
+  `,
   styleUrls: ['./cardtotal.component.scss']
 })
 export class CardtotalComponent implements OnInit {
+
+
+  @Input() number = "";
+  @Input() name = "";
+  @Input() color = "";
 
   constructor() { }
 
