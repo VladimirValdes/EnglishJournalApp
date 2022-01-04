600e9ebb00f5cfe4181e5e1882ae8ed251f1ad77
Vladimir <mendozahum20@gmail.com>
2022-01-04 11:09:35 -0600

    feat: add cardtotal module and component

---
+++ src/app/components/cardtotal/cardtotal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardtotal',
+  templateUrl: './cardtotal.component.html',
+  styleUrls: ['./cardtotal.component.scss']
+})
+export class CardtotalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/cardtotal/cardtotal.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardtotalComponent } from './cardtotal.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardtotalComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CardtotalComponent
+  ]
+})
+export class CardtotalModule { }
