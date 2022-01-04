68078ae81c5b800c10587e5a5a319453de4bd6ad
Vladimir <mendozahum20@gmail.com>
2022-01-04 11:53:08 -0600

    feat: add styles cardtotal

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { CardtotalModule } from '../components/cardtotal/cardtotal.module';
 
 
 
@@ -11,7 +12,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    CardtotalModule
   ],
   exports: [
     PagesComponent
