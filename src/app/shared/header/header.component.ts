860293bc87dae27c4d155263ac0588533cac7c53
Vladimir <mendozahum20@gmail.com>
2022-01-04 21:39:37 -0600

    fix: change searchbar with

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template:`
+    <header class="d-flex align-items-center  shadow bg-white mb-5">
+    <button
+      class="icon-menu"
+      (click)="activeMenu()">
+        <span class="icon-menu__bar">
+        </span>
+    </button>
+    <div class="searchbar my-4">
+        <input type="search" class="searchbar__input fs-4" placeholder="Search ..." aria-describedby="addon-wrapping">
+        <img src="/assets/icons/search.svg" alt="" class="searchbar__icon">
+    </div>
+</header>
+  `,
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
+
+  @Output() active = new EventEmitter<boolean>();
+  menuActive = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  activeMenu() {
+
+     this.menuActive = !this.menuActive;
+    this.active.emit(this.menuActive)
   }
 
 }
