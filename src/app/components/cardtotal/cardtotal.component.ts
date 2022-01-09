017398669dcd13a63c177db6b5473d8cc6214df8
Vladimir <mendozahum20@gmail.com>
2022-01-08 20:06:45 -0600

    feat: change sidebar animation and add cardtotal animation

--- src/app/components/cardtotal/cardtotal.component.ts
+++ src/app/components/cardtotal/cardtotal.component.ts
@@ -1,11 +1,27 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cardtotal',
+  animations: [
+    trigger('fadeSlideInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('500ms', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        animate('500ms', style({ opacity: 0, transform: 'translateY(-10px)' })),
+      ]),
+    ])
+  ],
   template: `
   <div class="d-flex justify-content-center align-items-center card shadow" style="width: 18rem; boder-radius: 5px">
     <div class="card-body">
-      <h5 class="card-title text-center fw-bolder" style="font-size: 5rem;" [style.color]="color">{{ number }}</h5>
+     <h5
+        [@fadeSlideInOut]
+        class="card-title text-center fw-bolder"
+        style="font-size: 5rem;"
+        [style.color]="color">{{ number }}</h5>
       <p class="card-text text-center fs-2" [style.color]="color">{{ name }}</p>
     </div>
   </div>
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -7,12 +7,10 @@ import { Component, OnInit } from '@angular/core';
     trigger(
       'enterAnimation', [
         transition(':enter', [
-          style({transform: 'translateX(0)', opacity: 0}),
-          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+          animate('.3s', style({transform: 'translateX(0)'}))
         ]),
         transition(':leave', [
-          style({transform: 'translateX(0)', opacity: 1}),
-          animate('0.3s ease-out', style({transform: 'translateX(-100%)', opacity: 0}))
+          animate('.3s', style({transform: 'translateX(-100%)'}))
         ])
       ]
     )
