d9ad9775089c8ce1891da1106aa99fcf7af542bd
Vladimir <mendozahum20@gmail.com>
2022-01-21 16:17:02 -0600

    feat: add login, readToken and saveToken method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { loginForm } from '../interfaces/loginForm.interface';
+import { map, Observable } from 'rxjs';
+import { User } from '../model/user.mode';
+
+
+const base_url = environment.base_url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+   public userToken: string = "";
+
+  constructor( private http: HttpClient) { }
+
+  login( formData: loginForm ): Observable< User > {
+    return this.http.post< User >(`${ base_url }/auth/login`, formData )
+                .pipe(
+                  map( ( res: User) => {
+                    this.saveToken( res.token );
+                      return res;
+                  })
+                )
+  }
+
+  private saveToken( token: string ) {
+    this.userToken = token;
+
+    localStorage.setItem('x-token', token );
+
+  }
 }
