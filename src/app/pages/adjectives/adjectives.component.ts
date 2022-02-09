f7378a2bc3c3109e373efb9f2b9f8240c2e5c425
Vladimir <mendozahum20@gmail.com>
2022-02-09 16:05:36 -0600

    feat: add CRUD on adjective component

--- src/app/pages/adjectives/adjectives.component.ts
+++ src/app/pages/adjectives/adjectives.component.ts
@@ -1,15 +1,152 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Observable, tap } from 'rxjs';
+import { Adjective } from 'src/app/interfaces/adjectives.interface';
+import { AdjectivesService } from 'src/app/services/adjectives.service';
+import { AlertsService } from 'src/app/services/alerts.service';
+import { ReportService } from 'src/app/services/report.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-adjectives',
   templateUrl: './adjectives.component.html',
-  styleUrls: ['./adjectives.component.scss']
+  styleUrls: ['./adjectives.component.scss'],
 })
 export class AdjectivesComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('closebtn') closebtn!: ElementRef;
+
+  @ViewChild('openModal') openModal!:ElementRef;
+
+  sumitted = false;
+
+  updateAdj = false;
+
+  idAdjective = '';
+
+
+  adjectives$!:Observable<Adjective[]>;
+
+
+  public adjectiveForm = this.fb.group({
+    adjective: ['', [ Validators.required, Validators.minLength(2)]],
+  },
+  {
+    updateOn: 'blur',
+  });
+
+  constructor( private fb: FormBuilder,
+    private adjectivesService: AdjectivesService,
+    private alertService: AlertsService,
+    private reportService: ReportService) { }
 
   ngOnInit(): void {
+    this.getAdjectives();
+  }
+
+  getAdjectives() {
+    this.adjectives$ = this.adjectivesService.getAdjectives();
+  }
+
+  addAdjective() {
+    this.sumitted = true;
+    if ( this.adjectiveForm.invalid) { return; }
+
+    console.log( this.adjectiveForm.get('adjective')?.value);
+    
+
+    if ( this.updateAdj ) {
+      this.adjectivesService.updateAdjective( this.adjectiveForm.value, this.idAdjective ).subscribe( () => {
+        this.alertService.success('Updated', 'Your adjective has been updated');
+        this.getAdjectives();
+      });
+
+    } else {
+      this.adjectivesService.addAdjective(this.adjectiveForm.value).subscribe( () => {
+        this.alertService.success('Created', 'Your adjective has been created');
+        this.getAdjectives();
+
+      });
+    }
+
+    this.closebtn.nativeElement.click();
+    this.close();
+
+  
+
+  }
+
+  selectAdjective( adjective: Adjective) {
+    console.log(adjective);
+    this.adjectiveForm.reset({
+      adjective: adjective.adjective,
+    });
+
+    this.idAdjective = adjective._id;
+    this.updateAdj = true;
+    this.openModal.nativeElement.click();
+    
+  }
+
+  deleteAdjective( id: string ) {
+    this.alertService.confirm().then( ( result ) => {
+      if ( result.isConfirmed ) {
+        this.adjectivesService.deleteAdjective(id).subscribe( () => {
+          this.alertService.success('Deleted', 'Your adjective has been deleted');
+          this.getAdjectives();
+        });
+      }
+    });
+    
+  }
+
+  generateReport() {
+    Swal.fire({
+      title: 'Loading PDF',
+    });
+    Swal.showLoading();
+    this.reportService.generateReport('adjectives', 'adjective' ).subscribe( resp => {
+      let fileURL = URL.createObjectURL( resp );      
+      window.open(fileURL);
+
+      Swal.close();
+      Swal.hideLoading();
+    });
+    
+  }
+
+  onSearch( term: string ) {
+
+    console.log({ term });
+    
+    if ( term ) {
+      this.adjectives$ = this.adjectivesService.searchAdjectives( term ).pipe(
+        tap( adjective => {
+          if ( adjective.length <= 0) {
+            this.alertService.info("We don't find any register");
+            this.getAdjectives();
+          }
+        }),
+      );
+    } else {
+      this.getAdjectives();
+    }
+  }
+
+  trackByFn( index: number): number {
+    return index;
+  }
+
+  invalidField(formControl: string): boolean {
+    const field = this.adjectiveForm.get(formControl);
+    return (field?.invalid && ( field?.touched || field?.dirty) && this.sumitted) ? true : false;
+  }
+
+  close( ) {
+    this.updateAdj = false;
+    this.adjectiveForm.reset({
+      adjective: '',
+    });
   }
 
 }
--- src/app/pages/adjectives/adjectives.module.ts
+++ src/app/pages/adjectives/adjectives.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdjectivesComponent } from './adjectives.component';
 import { adjectivesRouting } from './adjectivesRouting.module';
+import { SearchbarModule } from 'src/app/components/searchbar/searchbar.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    AdjectivesComponent
+    AdjectivesComponent,
   ],
   imports: [
     CommonModule,
-    adjectivesRouting
-  ]
+    adjectivesRouting,
+    SearchbarModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AdjectivesModule { }
